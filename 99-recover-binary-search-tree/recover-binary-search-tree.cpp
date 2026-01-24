83de41a3ac388f638e845747f3b5279ba0599155
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-01-24 19:12:46 +0530

    Time: 1 ms (26.85%) | Memory: 60.6 MB (99.80%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -1,69 +1,69 @@
-// /**
-//  * Definition for a binary tree node.
-//  * struct TreeNode {
-//  *     int val;
-//  *     TreeNode *left;
-//  *     TreeNode *right;
-//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-//  * };
-//  */
-// class Solution {
-// public:
-//     TreeNode* findInorderPredecessor(TreeNode* curr){
-//         auto temp = curr->left;
-//         while(temp->right && temp->right != curr){
-//             temp = temp->right;
-//         }
-//         return temp;
-//     }
-//     void recoverTree(TreeNode* root) {
-//         TreeNode* first = NULL;
-//         TreeNode* second = NULL;
-//         TreeNode* prev = NULL;
-//         auto curr = root;
-//         while(curr){
-//             if(curr->left == NULL){
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* findInorderPredecessor(TreeNode* curr){
+        auto temp = curr->left;
+        while(temp->right && temp->right != curr){
+            temp = temp->right;
+        }
+        return temp;
+    }
+    void recoverTree(TreeNode* root) {
+        TreeNode* first = NULL;
+        TreeNode* second = NULL;
+        TreeNode* prev = NULL;
+        auto curr = root;
+        while(curr){
+            if(curr->left == NULL){
 
-//                 //Do something
-//                 if(prev && curr->val < prev->val && !first && !second){
-//                     first = prev;
-//                     second = curr;
-//                 }
-//                 else if(prev && curr->val < prev->val && first && second){
-//                     second = curr;
-//                 }
-//                 prev = curr;
+                //Do something
+                if(prev && curr->val < prev->val && !first && !second){
+                    first = prev;
+                    second = curr;
+                }
+                else if(prev && curr->val < prev->val && first && second){
+                    second = curr;
+                }
+                prev = curr;
                 
-//                 curr = curr->right;
-//             }
-//             else{
-//                 auto IP = findInorderPredecessor(curr);
-//                 if(IP->right == NULL){
-//                     IP->right = curr;
-//                     curr = curr->left;
-//                 }
-//                 else{
-//                     IP->right = NULL;
+                curr = curr->right;
+            }
+            else{
+                auto IP = findInorderPredecessor(curr);
+                if(IP->right == NULL){
+                    IP->right = curr;
+                    curr = curr->left;
+                }
+                else{
+                    IP->right = NULL;
 
-//                     //Do something
-//                     if(prev && curr->val < prev->val && !first && !second){
-//                         first = prev;
-//                         second = curr;
-//                     }
-//                     else if(prev && curr->val < prev->val && first && second){
-//                         second = curr;
-//                     }
-//                     prev = curr;
+                    //Do something
+                    if(prev && curr->val < prev->val && !first && !second){
+                        first = prev;
+                        second = curr;
+                    }
+                    else if(prev && curr->val < prev->val && first && second){
+                        second = curr;
+                    }
+                    prev = curr;
 
-//                     curr = curr->right;
-//                 }
-//             }
-//         }
-//         if(first && second) swap(first->val, second->val);
-//     }
-// };
+                    curr = curr->right;
+                }
+            }
+        }
+        if(first && second) swap(first->val, second->val);
+    }
+};
 
 // class Solution {
 // public:
@@ -96,34 +96,34 @@
 //     }
 // };
 
-class Solution {
-public:
-    TreeNode* first = nullptr;
-    TreeNode* second = nullptr;
-    TreeNode* prev = nullptr;
+// class Solution {
+// public:
+//     TreeNode* first = nullptr;
+//     TreeNode* second = nullptr;
+//     TreeNode* prev = nullptr;
 
-    void inorder(TreeNode* root) {
-        if (!root) return;
+//     void inorder(TreeNode* root) {
+//         if (!root) return;
 
-        inorder(root->left);
+//         inorder(root->left);
 
-        // detect violation
-        if (prev && root->val < prev->val) {
-            if (!first) {
-                first = prev;
-                second = root;
-            } else {
-                second = root;
-            }
-        }
+//         // detect violation
+//         if (prev && root->val < prev->val) {
+//             if (!first) {
+//                 first = prev;
+//                 second = root;
+//             } else {
+//                 second = root;
+//             }
+//         }
 
-        prev = root;
+//         prev = root;
 
-        inorder(root->right);
-    }
+//         inorder(root->right);
+//     }
 
-    void recoverTree(TreeNode* root) {
-        inorder(root);
-        if(first&&second) swap(first->val, second->val);
-    }
-};
+//     void recoverTree(TreeNode* root) {
+//         inorder(root);
+//         if(first&&second) swap(first->val, second->val);
+//     }
+// };
