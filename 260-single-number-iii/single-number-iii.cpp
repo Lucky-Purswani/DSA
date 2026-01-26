53ccd4b90a713bf54cb8f149909b712bb760b671
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-01-26 14:35:20 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (58.30%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        vector<int> ans;
+        int xo = 0;
+        for(auto &i : nums){
+            xo ^= i;
+        }
+        int diff_bit = 1;
+        int a = 0, b = 0;
+        while((xo & diff_bit) == 0){
+            diff_bit <<= 1;
+        }
+        //Or instead of while loop
+        // diff_bit = (xo & (xo-1)) ^ xo;
+
+        for(auto &i : nums){
+            if((diff_bit & i) == 0) b ^= i;
+            else a ^= i;
+        }
+        return {a, b};
+    }
+};
