8f92938fe8efa6a991c97769576bb9d92f130365
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-02-09 14:06:35 +0530

    Time: 33 ms (41.08%) | Memory: 61.7 MB (8.52%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,25 +1,56 @@
-//Recursion + DP
+//Tabulation
 class Solution {
 public:
-    int solve(vector<int> coins, int sum, int n, vector<vector<int>> &dp){
-        if(sum == 0) return 1;
-        if(n == 0) return 0;
-        if(dp[n][sum] != -1) return dp[n][sum];
-        if(sum >= coins[n-1]){
-            int np = solve(coins, sum, n-1, dp);
-            int p = solve(coins, sum-coins[n-1], n, dp);
-            return dp[n][sum] = p+np;
-        }
-        else{
-            int np = solve(coins, sum, n-1, dp);
-            return dp[n][sum] = np;
+    int change(int sum, vector<int>& coins) {
+        int n = coins.size();
+        // Use long long to avoid overflow
+        vector<vector<unsigned long long>> dp(n+1, vector<unsigned long long>(sum+1, 0));
+        
+        // Base case: sum 0 can always be made with 0 coins
+        for(int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
         }
         
-    }
-    int change(int sum, vector<int>& coins) {
-        if(sum == 0) return 1;
-        vector<vector<int>> dp(coins.size()+1, vector<int>(sum+1, -1));
-        int ans = solve(coins, sum, coins.size(), dp);
-        return dp[coins.size()][sum];
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= sum; j++) {
+                if(j >= coins[i-1]) {
+                    unsigned long long np = dp[i-1][j];           // not pick
+                    unsigned long long p  = dp[i][j - coins[i-1]]; // pick
+                    dp[i][j] = p + np;
+                } else {
+                    dp[i][j] = dp[i-1][j]; // cannot pick
+                }
+            }
+        }
+        
+        return dp[n][sum];
     }
 };
+
+
+
+// //Recursion + DP
+// class Solution {
+// public:
+//     int solve(vector<int> coins, int sum, int n, vector<vector<int>> &dp){
+//         if(sum == 0) return 1;
+//         if(n == 0) return 0;
+//         if(dp[n][sum] != -1) return dp[n][sum];
+//         if(sum >= coins[n-1]){
+//             int np = solve(coins, sum, n-1, dp);
+//             int p = solve(coins, sum-coins[n-1], n, dp);
+//             return dp[n][sum] = p+np;
+//         }
+//         else{
+//             int np = solve(coins, sum, n-1, dp);
+//             return dp[n][sum] = np;
+//         }
+        
+//     }
+//     int change(int sum, vector<int>& coins) {
+//         if(sum == 0) return 1;
+//         vector<vector<int>> dp(coins.size()+1, vector<int>(sum+1, -1));
+//         int ans = solve(coins, sum, coins.size(), dp);
+//         return dp[coins.size()][sum];
+//     }
+// };
