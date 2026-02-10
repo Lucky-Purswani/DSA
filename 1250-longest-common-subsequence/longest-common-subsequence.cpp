80b8ca043e88113662ea4baf447f512562b13fe0
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-02-10 21:32:42 +0530

    Time: 17 ms (87.52%) | Memory: 9 MB (99.19%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -3,23 +3,47 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.length();
         int n = text2.length();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        vector<int> prev(n+1, 0);
+        vector<int> curr(n+1, 0);
         
-        //No need of initialization loops as we want 0 so we have 0.
+        //No need of initialization of prev or curr as we want 0 so we have 0.
 
         for(int i = 1; i<=m; i++){
             for(int j = 1; j<=n; j++){
                 if(text1[i-1] == text2[j-1]){
-                    dp[i][j] = 1+dp[i-1][j-1];
+                    curr[j] = 1+prev[j-1];
                 }
                 else{
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    curr[j] = max(prev[j], curr[j-1]);
                 }
             }
+            prev = curr;
         }
-        return dp[m][n];
+        return prev[n];
     }
 };
+// class Solution {
+// public:
+//     int longestCommonSubsequence(string text1, string text2) {
+//         int m = text1.length();
+//         int n = text2.length();
+//         vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        
+//         //No need of initialization loops as we want 0 so we have 0.
+
+//         for(int i = 1; i<=m; i++){
+//             for(int j = 1; j<=n; j++){
+//                 if(text1[i-1] == text2[j-1]){
+//                     dp[i][j] = 1+dp[i-1][j-1];
+//                 }
+//                 else{
+//                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+//                 }
+//             }
+//         }
+//         return dp[m][n];
+//     }
+// };
 // class Solution {
 // public:
 //     int solve(string &x, string &y, int m, int n, vector<vector<int>> &dp){
