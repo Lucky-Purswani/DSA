59662006dfe517f4274039b89c017532320789d3
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-02-12 10:48:24 +0530

    Time: 688 ms (5.01%) | Memory: 380.9 MB (5.01%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -0,0 +1,23 @@
+//Recursion + Memoization
+class Solution {
+public:
+    int solve(string x, string y, int m, int n, vector<vector<int>> &dp){
+        if (m == 0) return n;
+        if (n == 0) return m;
+        if(dp[m][n] != -1) return dp[m][n];
+
+        if(x[m-1] == y[n-1]){
+            return dp[m][n] = solve(x, y, m-1, n-1, dp);
+        }
+        else{
+            return dp[m][n] = 1+min(solve(x, y, m-1, n, dp), solve(x, y, m, n-1, dp));
+        }
+    }
+    int minDistance(string x, string y) {
+        int m = x.length();
+        int n = y.length();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        int ans = solve(x, y, m, n, dp);
+        return ans;
+    }
+};
