94218814723425658f1d558e977674c19482fd3e
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-02-12 11:02:29 +0530

    Time: 5 ms (91.44%) | Memory: 11.3 MB (95.32%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,27 +1,80 @@
-//Tabulation
+//Space optimization
 class Solution {
 public:
     int minDistance(string x, string y) {
         int m = x.length();
         int n = y.length();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        vector<int> prev(n+1, 0);
+        vector<int> curr(n+1, 0);
 
-        for(int i = 0; i<=m; i++) dp[i][0] = i;
-        for(int j = 0; j<=n; j++) dp[0][j] = j;
+        for(int i = 0; i<=n; i++) prev[i] = i;
+        for(int j = 0; j<=n; j++) curr[j] = j;
 
         for(int i = 1; i<=m; i++){
+            curr[0] = i;
             for(int j = 1; j<=n; j++){
                 if(x[i-1] == y[j-1]){
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 }
                 else{
-                    dp[i][j] = 1+min(dp[i][j-1], dp[i-1][j]);
+                    curr[j] = 1+min(curr[j-1], prev[j]);
                 }
             }
+            prev = curr;
         }
-        return dp[m][n];
+        return curr[n];
     }
 };
+
+
+// //Tabulation
+// class Solution {
+// public:
+//     int minDistance(string x, string y) {
+//         int m = x.length();
+//         int n = y.length();
+//         vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+
+//         for(int i = 0; i<=m; i++) dp[i][0] = i;
+//         for(int j = 0; j<=n; j++) dp[0][j] = j;
+
+//         for(int i = 1; i<=m; i++){
+//             for(int j = 1; j<=n; j++){
+//                 if(x[i-1] == y[j-1]){
+//                     dp[i][j] = dp[i-1][j-1];
+//                 }
+//                 else{
+//                     dp[i][j] = 1+min(dp[i][j-1], dp[i-1][j]);
+//                 }
+//             }
+//         }
+//         //For printing the string
+//         // string result = "";
+//         // int i = m, j = n;
+
+//         // while(i > 0 && j > 0){
+//         //     if(x[i-1] == y[j-1]){
+//         //         result.push_back(x[i-1]);
+//         //         i--;
+//         //         j--;
+//         //     }
+//         //     else{
+//         //         if(dp[i-1][j] < dp[i][j-1])
+//         //             i--;
+//         //         else
+//         //             j--;
+//         //     }
+//         // }
+
+//         // reverse(result.begin(), result.end());
+//         // cout << result << endl;
+//         // return result;
+
+
+
+//         return dp[m][n];
+//     }
+// };
 
 
 // //Recursion + Memoization
