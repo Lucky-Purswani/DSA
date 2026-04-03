a3dcfbdc7c32660bfe6e2caad22c8a67f64ed4b8
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-04-03 12:16:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (21.38%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    int sum(vector<int> &a, int i, vector<int> &dp){
-        if(i < 0) return 0;
-        if(i == 0) return a[i];
-        if(dp[i] != -1) return dp[i];
-        int p = a[i] + sum(a, i-2, dp);
-        int np = sum(a, i-1, dp);
-        
-        return dp[i] = max(p, np);
-        
-    }
 
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        int ans = sum(nums, nums.size()-1, dp);
-        return ans;
+        int n = nums.size();
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        int p = 0, np=0;
+        for(int i = 1; i<n; i++){
+            int p = nums[i];
+            if(i-2>=0) p += dp[i-2];
+            int np = dp[i-1];
+            dp[i] = max(p, np);
+        }
+        return dp[n-1];
     }
 };
