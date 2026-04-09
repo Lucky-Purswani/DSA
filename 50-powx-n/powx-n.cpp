e3739c8ee8e4f1c708f1c263e1e14ec3f1ea948f
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-04-09 10:04:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (43.83%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,9 +1,6 @@
 class Solution {
     public: double myPow(double x, int n) {
         long N = n;
-        if (N < 0) {
-            return 1.0 / power(x, -N);
-        }
         return power(x, N);
     }
 
@@ -14,6 +11,9 @@ class Solution {
         if (n == 1) {
             return x;
         }
+
+        if (n < 0) return 1.0 / power(x, -n);
+
         if (n % 2 == 0) {
             return power(x * x, n / 2);
         }
