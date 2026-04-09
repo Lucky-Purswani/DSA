fdbb14318173ddf7ce88ca0726a45f1706c0ae51
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-04-09 15:19:53 +0530

    Time: 0 ms (100.00%) | Memory: 25.1 MB (76.07%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int i = 0;
+        int n = nums.size();
+
+        // Cyclic Sort
+        while (i < n) {
+            int corr = nums[i] - 1;
+            if (nums[i] != nums[corr]) {
+                swap(nums[i], nums[corr]);
+            } else {
+                i++;
+            }
+        }
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                ans.push_back(nums[i]);
+                ans.push_back(i+1);
+            }
+        }
+        return ans;
+    }
+};
