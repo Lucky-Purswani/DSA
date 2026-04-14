a15f058cae829870e17cfe868adc8898389b39b2
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-04-14 15:53:33 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (62.90%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -2,17 +2,25 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         if(strs.empty()) return "";
+        int len = 0;
+        string ans;
+        for(auto &w:strs){
+            if(w.size() > len){
+                len = w.size();
+                ans = w;
+            }
+        }
+        for(int i = 0; i < len; i++){
+            char ch = strs[0][i];
 
-        sort(strs.begin(), strs.end());
-
-        string first = strs[0];
-        string last = strs.back();
-
-        int i = 0;
-        while(i < last.size() && first[i] == last[i]){
-            i++;
+            for(int j = 1; j < strs.size(); j++){
+                // mismatch or string ended
+                if(strs[j][i] != ch){
+                    return strs[0].substr(0, i);
+                }
+            }
         }
 
-        return first.substr(0, i);
+        return ans;
     }
 };
