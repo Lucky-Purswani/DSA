606243ebac27257b6ae19d776cc9eb22da5cac80
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-04-14 17:06:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (84.70%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int ans = 0;
+        int cnt = 0;
+        for(auto &i: s){
+            if(i == '('){
+                cnt++;
+                ans = max(ans, cnt);
+            }
+            if(i == ')'){
+                cnt--;
+            }
+        }
+        return ans;
+    }
+};
