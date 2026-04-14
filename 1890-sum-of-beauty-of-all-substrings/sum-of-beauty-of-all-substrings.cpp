06fe7d6bee64da33d1ee2e0e4712ce21a34e6df2
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-04-14 18:59:50 +0530

    Time: 57 ms (89.41%) | Memory: 11.1 MB (63.93%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -1,3 +1,29 @@
+// class Solution {
+// public:
+//     int beauty(string s){
+//         if(s.length() <= 1) return 0;
+//         unordered_map<char, int> mp;
+//         for(auto &ch: s){
+//             mp[ch]++;
+//         }
+//         int mn = INT_MAX;
+//         int mx = INT_MIN;
+//         for(auto &p: mp){
+//             mn = min(mn, p.second);
+//             mx = max(mx, p.second);
+//         }
+//         return mx-mn;
+//     }
+//     int beautySum(string s) {
+//         int sum = 0;
+//         for(int i = 0; i<s.length(); i++){
+//             for(int j = i; j<s.length(); j++){
+//                 sum += beauty(s.substr(i, j-i+1));
+//             }
+//         }
+//         return sum;
+//     }
+// };
 class Solution {
 public:
 
