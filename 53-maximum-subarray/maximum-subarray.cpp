d915fe17e2c2f2f4f2e5b057d469c031e9a3a641
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-04-19 10:41:58 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (51.65%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int ans = INT_MIN;
-        int sum = 0;
-        for(int i = 0; i<nums.size(); i++){
-            sum += nums[i];
-            ans = max(ans, sum);
-            if(sum < 0) sum = 0;
+        int ans = nums[0];
+        int best = nums[0];
+        for(int i = 1; i<nums.size(); i++){
+            int v1 = nums[i];
+            int v2 = nums[i]+best;
+            best = max(v1, v2);
+            ans = max(ans, best);
+            
         }
         return ans;
     }
 };
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+//         int ans = INT_MIN;
+//         int sum = 0;
+//         for(int i = 0; i<nums.size(); i++){
+//             sum += nums[i];
+//             ans = max(ans, sum);
+//             if(sum < 0) sum = 0;
+//         }
+//         return ans;
+//     }
+// };
