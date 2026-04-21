8e03daa0ba026c0f4253f44cb1179d3b02729830
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-04-21 15:08:16 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (92.80%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -4,7 +4,7 @@ public:
         int n = matrix.size();
 
         // transpose
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n-1; i++) {
             for(int j = i + 1; j < n; j++) {
                 swap(matrix[i][j], matrix[j][i]);
             }
