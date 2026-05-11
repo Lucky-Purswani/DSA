4a17fe1c2360b5673edee41d5c2b8468f6f91509
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-05-11 19:10:29 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (59.79%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,38 +1,42 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        int i = 0;
-        int j = 0;
-        while(i<n1 && j<n2){
-            if(nums1[i] <= nums2[j]){
-                ans.push_back(nums1[i]);
+    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
+        int i = 0, j = 0, count = 0;
+        int n1 = a.size(), n2 = b.size(), n = n1 + n2;
+        int index1 = n / 2, index2 = (n % 2 == 0) ? index1 - 1 : -1;
+        int index1el = -1, index2el = -1;
+
+        while (i < n1 && j < n2) {
+            int val;
+            if(a[i] < b[j]){
+                val = a[i];
                 i++;
             }
             else{
-                ans.push_back(nums2[j]);
+                val = b[j];
                 j++;
             }
+            if(count == index1) index1el = val;
+            if(count == index2) index2el = val;
+            count++;
         }
-        while(i < n1){
-            ans.push_back(nums1[i]);
+
+        while (i < n1) {
+            int val = a[i];
             i++;
+            if(count == index1) index1el = val;
+            if(count == index2) index2el = val;
+            count++;
         }
-        while(j < n2){
-            ans.push_back(nums2[j]);
+
+        while (j < n2) {
+            int val = b[j];
             j++;
+            if(count == index1) index1el = val;
+            if(count == index2) index2el = val;
+            count++;
         }
 
-        for(auto &i:ans){
-            cout << i << " ";
-        }
-
-        if(ans.size() % 2 == 0){
-            double med = ((double)ans[ans.size()/2] + ans[(ans.size()/2) - 1])/2;
-            return med;
-        }
-        return ans[ans.size()/2];
+        return (n % 2 == 0) ? (double(index1el + index2el) / 2) : index1el;
     }
 };
