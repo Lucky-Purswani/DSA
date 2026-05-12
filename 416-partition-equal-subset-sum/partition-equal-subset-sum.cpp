7c9726c872b0c89dee96396e4d9bf280bbcd7595
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-05-12 12:30:31 +0530

    Time: 169 ms (22.72%) | Memory: 110.6 MB (23.12%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
 
-    bool isValid(vector<int>& nums, long long sum, long long currSum, int i, vector<vector<int>> &dp){
+    bool isValid(vector<int>& nums, long long sum, long long currSum,
+                 int i, vector<vector<int>> &dp){
 
-        if(i < 0) return (currSum == sum/2);
+        if(currSum > sum/2) return false;
 
         if(currSum == sum/2) return true;
-        if(currSum > sum/2) return false;
+
+        if(i < 0) return false;
 
         if(dp[i][currSum] != -1) return dp[i][currSum];
 
@@ -26,8 +28,11 @@ public:
         }
 
         if(sum % 2) return false;
+
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(sum/2+1, -1));
-        return isValid(nums, sum, 0, nums.size() - 1, dp);
+
+        vector<vector<int>> dp(n, vector<int>(sum/2 + 1, -1));
+
+        return isValid(nums, sum, 0, n - 1, dp);
     }
 };
