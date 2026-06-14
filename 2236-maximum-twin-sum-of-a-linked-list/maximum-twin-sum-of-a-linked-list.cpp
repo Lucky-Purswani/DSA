87c47ba9be2fda6611267455fe3c5fee7b227ea5
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-06-14 19:06:51 +0530

    Time: 4 ms (56.70%) | Memory: 124.2 MB (85.28%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -10,21 +10,58 @@
  */
 class Solution {
 public:
-    int pairSum(ListNode* head) {
-        vector<int> nodes;
-        auto temp = head;
-        while(temp){
-            nodes.push_back(temp->val);
-            temp = temp->next;
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while (curr != NULL) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
+
+        return prev;
+    }
+    int pairSum(ListNode* head) {
+        auto s = head;
+        auto f = head;
+        while(f && f->next){
+            s = s->next;
+            f = f->next->next;
+        }
+        auto endN = reverse(s);
+        auto startN = head;
         long long sum = 0;
-        int i = 0;
-        int j = nodes.size()-1;
-        while(i<j){
-            sum = max(sum, 1LL * nodes[i] + nodes[j]);
-            i++;
-            j--;
+
+        while(endN){
+            sum = max(sum, 1LL * startN->val + endN->val);
+            endN = endN->next;
+            startN = startN->next;
         }
         return sum;
     }
 };
+
+
+//Extra Space.
+// class Solution {
+// public:
+//     int pairSum(ListNode* head) {
+//         vector<int> nodes;
+//         auto temp = head;
+//         while(temp){
+//             nodes.push_back(temp->val);
+//             temp = temp->next;
+//         }
+//         long long sum = 0;
+//         int i = 0;
+//         int j = nodes.size()-1;
+//         while(i<j){
+//             sum = max(sum, 1LL * nodes[i] + nodes[j]);
+//             i++;
+//             j--;
+//         }
+//         return sum;
+//     }
+// };
