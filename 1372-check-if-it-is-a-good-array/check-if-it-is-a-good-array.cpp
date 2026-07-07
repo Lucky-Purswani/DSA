280f1dea53f8e3ed6d8c8405c3ac3d72367de565
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-07-07 19:23:03 +0530

    Time: 4 ms (29.38%) | Memory: 33.1 MB (21.33%) - LeetSync

---
+++ 1372-check-if-it-is-a-good-array/check-if-it-is-a-good-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isGoodArray(vector<int>& nums) {
+        int gcd = nums[0];
+        for (int num : nums) {
+            gcd = __gcd(gcd, num);
+            if (gcd == 1) return true;
+        }
+        return false;
+    }
+};
