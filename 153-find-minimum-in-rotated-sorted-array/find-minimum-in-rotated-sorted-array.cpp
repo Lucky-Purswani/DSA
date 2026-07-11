93467d9fbdc18a7b2a146829e5fa85413a2a0cb9
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-07-11 22:55:46 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (47.47%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,25 +1,46 @@
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
-        int n = nums.size();
-        int s = 0;
-        int e = n-1;
-        int idx = 0;
-        int ans = INT_MAX;
-        while(s <= e){
-            int m = s + (e-s)/2;
-            if(nums[m] >= nums[s]){
-                //left one is sorted
-                ans = min(ans, nums[s]);
-                idx = s;
-                s = m+1;
-            }
-            else{
-                idx = m;
-                ans = min(ans, nums[m]);
-                e = m-1;
-            }
+    int smallest(vector<int> &nums, int s, int e,  int &idx){
+        if(s > e) return idx;
+
+        int m = s + (e-s)/2;
+        if(nums[m] >= nums[s]){
+            if(idx == -1 || nums[idx] > nums[s])idx = s;
+            return smallest(nums, m+1, e, idx);
         }
-        return ans;
+        else{
+            if(idx == -1 || nums[idx] > nums[m])idx = m;
+            return smallest(nums, s, m-1, idx);
+        }
+    }
+    int findMin(vector<int>& nums) {
+        int i = -1;
+        int idx = smallest(nums, 0, nums.size()-1, i);
+        return nums[idx];
     }
 };
+// class Solution {
+// public:
+//     int findMin(vector<int>& nums) {
+//         int n = nums.size();
+//         int s = 0;
+//         int e = n-1;
+//         int idx = 0;
+//         int ans = INT_MAX;
+//         while(s <= e){
+//             int m = s + (e-s)/2;
+//             if(nums[m] >= nums[s]){
+//                 //left one is sorted
+//                 ans = min(ans, nums[s]);
+//                 idx = s;
+//                 s = m+1;
+//             }
+//             else{
+//                 idx = m;
+//                 ans = min(ans, nums[m]);
+//                 e = m-1;
+//             }
+//         }
+//         return ans;
+//     }
+// };
