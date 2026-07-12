6cbdf939867896b5044d1c481367af642b2d377b
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-07-12 13:38:20 +0530

    Time: 15 ms (28.93%) | Memory: 169.4 MB (65.20%) - LeetSync

--- 4351-minimum-total-cost-to-process-all-elements/minimum-total-cost-to-process-all-elements.cpp
+++ 4351-minimum-total-cost-to-process-all-elements/minimum-total-cost-to-process-all-elements.cpp
@@ -5,53 +5,18 @@ public:
         const ll MOD = 1e9 + 7;
 
         ll res = k;
-        ll ans = 0;
-        ll cnt = 0; 
+        ll cnt = 0;
 
         for (int x : nums) {
             if (res < x) {
                 ll need = x - res;
-
-                
-                ll temp = (need + k - 1) / k;
-
-                cnt += temp;
-                res += temp * 1LL * k;
+                ll buys = (need + k - 1) / k;
+                cnt += buys;
+                res += buys * k;
             }
-
             res -= x;
         }
+
         return (ll)(((__int128)cnt * (cnt + 1) / 2) % MOD);
     }
 };
-// class Solution {
-// public:
-//     int minimumCost(vector<int>& nums, int k) {
-//         using ll = long long;
-//         const ll MOD = 1e9 + 7;
-
-//         ll resource = k;
-//         ll ans = 0;
-//         ll id = 0; 
-
-//         for (int x : nums) {
-//             if (resource < x) {
-//                 ll need = x - resource;
-
-                
-//                 ll cnt = (need + k - 1) / k;
-
-//                 __int128 cost = (__int128)cnt * (2LL * id + cnt + 1) / 2;
-
-//                 ans = (ans + (long long)(cost % MOD)) % MOD;
-
-//                 id += cnt;
-//                 resource += cnt * 1LL * k;
-//             }
-
-//             resource -= x;
-//         }
-
-//         return (int)ans;
-//     }
-// };
