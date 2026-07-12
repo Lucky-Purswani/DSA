2f4cd243f0ab3bfc70553536deffe792544c7132
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-07-12 13:25:26 +0530

    Time: 12 ms (41.07%) | Memory: 169.3 MB (95.65%) - LeetSync

--- 4351-minimum-total-cost-to-process-all-elements/minimum-total-cost-to-process-all-elements.cpp
+++ 4351-minimum-total-cost-to-process-all-elements/minimum-total-cost-to-process-all-elements.cpp
@@ -4,28 +4,54 @@ public:
         using ll = long long;
         const ll MOD = 1e9 + 7;
 
-        ll resource = k;
+        ll res = k;
         ll ans = 0;
-        ll id = 0; 
+        ll cnt = 0; 
 
         for (int x : nums) {
-            if (resource < x) {
-                ll need = x - resource;
+            if (res < x) {
+                ll need = x - res;
 
                 
-                ll cnt = (need + k - 1) / k;
+                ll temp = (need + k - 1) / k;
 
-                __int128 cost = (__int128)cnt * (2LL * id + cnt + 1) / 2;
-
-                ans = (ans + (long long)(cost % MOD)) % MOD;
-
-                id += cnt;
-                resource += cnt * 1LL * k;
+                cnt += temp;
+                res += temp * 1LL * k;
             }
 
-            resource -= x;
+            res -= x;
         }
-
-        return (int)ans;
+        return (ll)(((__int128)cnt * (cnt + 1) / 2) % MOD);
     }
 };
+// class Solution {
+// public:
+//     int minimumCost(vector<int>& nums, int k) {
+//         using ll = long long;
+//         const ll MOD = 1e9 + 7;
+
+//         ll resource = k;
+//         ll ans = 0;
+//         ll id = 0; 
+
+//         for (int x : nums) {
+//             if (resource < x) {
+//                 ll need = x - resource;
+
+                
+//                 ll cnt = (need + k - 1) / k;
+
+//                 __int128 cost = (__int128)cnt * (2LL * id + cnt + 1) / 2;
+
+//                 ans = (ans + (long long)(cost % MOD)) % MOD;
+
+//                 id += cnt;
+//                 resource += cnt * 1LL * k;
+//             }
+
+//             resource -= x;
+//         }
+
+//         return (int)ans;
+//     }
+// };
