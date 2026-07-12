a5c237f7311eaa7b897e29859f719cdce002d816
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-07-12 10:23:14 +0530

    Time: 15 ms (28.93%) | Memory: 169.5 MB (35.94%) - LeetSync

---
+++ 4351-minimum-total-cost-to-process-all-elements/minimum-total-cost-to-process-all-elements.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& nums, int k) {
+        using ll = long long;
+        const ll MOD = 1e9 + 7;
+
+        ll resource = k;
+        ll ans = 0;
+        ll id = 0; 
+
+        for (int x : nums) {
+            if (resource < x) {
+                ll need = x - resource;
+
+                
+                ll cnt = (need + k - 1) / k;
+
+                __int128 cost = (__int128)cnt * (2LL * id + cnt + 1) / 2;
+
+                ans = (ans + (long long)(cost % MOD)) % MOD;
+
+                id += cnt;
+                resource += cnt * 1LL * k;
+            }
+
+            resource -= x;
+        }
+
+        return (int)ans;
+    }
+};
