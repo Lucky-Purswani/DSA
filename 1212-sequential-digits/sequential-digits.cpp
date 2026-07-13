0413762d569c3934163d1e4ba04f55c24550acf6
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-07-13 15:32:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (18.24%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -3,20 +3,21 @@ public:
     vector<int> sequentialDigits(int low, int high) {
         int lowDig = floor(log10(low)) + 1;
         int highDig = floor(log10(high)) + 1;
+
         string s = "123456789";
         vector<int> ans;
-        for(int i = 1; i<=9; i++){
-            bool push = false;
-            for(int j = lowDig; j<=highDig; j++){
-                if(i-1+j > 9) break;
-                string temp = s.substr(i-1, j);
 
-                if(temp.length() > 0 && stoi(temp) >= low && stoi(temp) <= high){
-                    ans.push_back(stoi(temp));
-                    push = true;
-                }
+        for(int i = 0; i < 9; i++) {
+            for(int len = lowDig; len <= highDig; len++) {
+                if(i + len > 9) break;
+
+                int num = stoi(s.substr(i, len));
+
+                if(num >= low && num <= high)
+                    ans.push_back(num);
             }
         }
+
         sort(ans.begin(), ans.end());
         return ans;
     }
