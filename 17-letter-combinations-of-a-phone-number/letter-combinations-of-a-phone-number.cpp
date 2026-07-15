6c043daea01f414bdd8df2c5160cb623c8fd1fa7
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-07-15 10:18:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (21.10%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> ans;
+    void solve(string d, int i, unordered_map<char, string> &mp, string temp){
+        if(i == d.length()) {
+            ans.push_back(temp);
+            return;
+        }
+
+        cout << temp << " ";
+        string atI = mp[d[i]]; //"abc"
+        for(int j = 0; j<atI.length(); j++){
+            solve(d, i+1, mp, temp+atI[j]);
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty()) return {};
+        unordered_map<char, string> mp = {
+            {'2', "abc"},
+            {'3', "def"},
+            {'4', "ghi"},
+            {'5', "jkl"},
+            {'6', "mno"},
+            {'7', "pqrs"},
+            {'8', "tuv"},
+            {'9', "wxyz"}
+        };
+        solve(digits, 0, mp, "");
+        return ans;
+    }
+};
