1e255e8173e0588a035262cf4594a158516727c8
Purswani Lucky Suresh <luckyspurswani2408@gmail.com>
2026-07-21 22:19:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (38.03%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int steps(int n, vector<int> &dp){
+        if(n == 0) return 1;
+        if(n < 0) return 0;
+        if(dp[n] != -1) return dp[n];
+        int left = steps(n-1, dp);
+        int right = steps(n-2, dp);
+
+        return dp[n] = left+right;
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        return steps(n, dp);
+    }
+};
